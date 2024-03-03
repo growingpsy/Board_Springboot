8b60f03133e06fb203576da65abd4c8af5f3f01d
psy <psy915@swu.ac.kr>
2024-03-03 23:29:37 +0900

    게시글 작성 데이터 컨트롤러에 가져오기

--- src/main/java/com/psy/board/controller/BoardController.java
+++ src/main/java/com/psy/board/controller/BoardController.java
@@ -1,9 +1,12 @@
 package com.psy.board.controller;
 
+import com.psy.board.dto.BoardDTO;
 import com.psy.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor //final이 붙은 필드만 가지고 생성자를 만들겠다.
@@ -13,6 +16,12 @@ public class BoardController {
     @GetMapping("/save") //주소
     public String save() { //자바 메서드 이름
         return "save"; //리턴 화면
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) { //BoardDTO앞에 @ModelAttribute 어노테이션 생략가능
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
     }
 }
 
